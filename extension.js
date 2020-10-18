35e1b5fc6c732d7c4ae581ffdba2ca8bf9b0de6e
dongzhonghua03 <dongzhonghua03@kuaishou.com>
2020-10-18 23:51:49 +0800

    fix

--- extension.js
+++ extension.js
@@ -46,11 +46,11 @@ function disposeRemotePic() {
 			const pic_url = url.text;
 			// upload(res + ".png", url.text);
 			console.log('name: ' + pic_name + ', url: ' + pic_url);
-			axios.get(pic_url,
+			axios.get(encodeURI(pic_url),
 				{
 					responseType: 'arraybuffer'
 				})
-				.then(res => upload(pic_name, Buffer.from(res.data).toString('base64')))
+				.then(res => uploadGitee(pic_name, Buffer.from(res.data).toString('base64')))
 				.catch(function (error) {
 					console.log(error);
 					vscode.window.showInformationMessage("download image failed, please try again!");
@@ -74,30 +74,69 @@ function disposeLocalPic() {
 			const base64Data = Buffer.from(data).toString('base64');
 			const paths = path.split("/");
 			const pic_name = paths[paths.length - 1];
-			upload(pic_name, base64Data);
+			uploadGitee(pic_name, base64Data);
 		});
 }
+
+// /**
+//  * @param {string} pic_name
+//  * @param {string} base64_data
+//  */
+// function upload(pic_name, base64_data) {
+// 	const url = vscode.workspace.getConfiguration().get('pic.github_url');
+// 	const name = vscode.workspace.getConfiguration().get('pic.github_name');
+// 	const email = vscode.workspace.getConfiguration().get('pic.github_email');
+// 	const auth = vscode.workspace.getConfiguration().get('pic.github_authorization');
+// 	console.log(auth);
+// 	const headers = { 'content-type': 'application/json', 'Authorization': auth };
+// 	const jdata = JSON.stringify({
+// 		"message": pic_name,
+// 		"committer": {
+// 			"name": name,
+// 			"email": email
+// 		},
+// 		"content": base64_data
+// 	});
+// 	axios.put(url + pic_name, jdata, {
+// 		headers: headers
+// 	}
+// 	).then(res => {
+// 		console.log(res);
+// 		if (res.status = 200) {
+// 			const git_url = res.data["content"]["download_url"]
+// 			console.log(git_url);
+// 			vscode.window.activeTextEditor.edit(editBuilder => {
+// 				const md_img = "![" + pic_name + "](" + git_url + ")";
+// 				const end = new vscode.Position(vscode.window.activeTextEditor.selection.active.line + 1, 0);
+// 				editBuilder.replace(new vscode.Range(new vscode.Position(vscode.window.activeTextEditor.selection.active.line, 0), end), md_img);
+// 			});
+// 		} else {
+// 			console.error("upload url failed!")
+// 			vscode.window.showInformationMessage("upload url failed, please try again!");
+// 		}
+// 	}).catch(function (error) {
+// 		console.log(error);
+// 		vscode.window.showInformationMessage("upload url failed, please try again!");
+// 	});
+// }
 
 /**
  * @param {string} pic_name
  * @param {string} base64_data
  */
-function upload(pic_name, base64_data) {
-	const url = vscode.workspace.getConfiguration().get('pic.github_url');
-	const name = vscode.workspace.getConfiguration().get('pic.github_name');
-	const email = vscode.workspace.getConfiguration().get('pic.github_email');
-	const auth = vscode.workspace.getConfiguration().get('pic.github_authorization');
-	console.log(auth);
-	const headers = { 'content-type': 'application/json', 'Authorization': auth };
+function uploadGitee(pic_name, base64_data) {
+	const url = vscode.workspace.getConfiguration().get('pic.git');
+	const auth = vscode.workspace.getConfiguration().get('pic.access_token');
+	const headers = { 'Content-Type': 'application/json', 'charset': 'UTF-8' };
 	const jdata = JSON.stringify({
+		"access_token": auth,
 		"message": pic_name,
-		"committer": {
-			"name": name,
-			"email": email
-		},
 		"content": base64_data
 	});
-	axios.put(url + pic_name, jdata, {
+	console.log(headers)
+	console.log(jdata);
+
+	axios.post(encodeURI(url + pic_name), jdata, {
 		headers: headers
 	}
 	).then(res => {
@@ -118,4 +157,5 @@ function upload(pic_name, base64_data) {
 		console.log(error);
 		vscode.window.showInformationMessage("upload url failed, please try again!");
 	});
+
 }
